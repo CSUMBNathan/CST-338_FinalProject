ccc6d4066ba4ccb1aaeb566ae429255487d5218e
CSUMBNathan <ntrujillo@csumb.edu>
2023-11-17 13:21:47 -0800

    Added UserDao

---
+++ app/src/main/java/com/example/taskmanager/UserDao.java
@@ -0,0 +1,18 @@
+package com.example.taskmanager;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface UserDao {
+    @Insert
+    void insert(User user);
+
+    @Query("SELECT * FROM user_table")
+    List<User> getAllUsers();
+
+}
+
+
